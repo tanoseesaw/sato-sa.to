83717d476ec3709782dc6f0abbc5914de0e1d256
masarakki <masaki182@gmail.com>
2020-08-23 14:56:23 +0900

    add-nav-icon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 require('./stylesheets/index.scss');
+
+window.addEventListener('load', () => {
+  document.querySelector('#party-button').addEventListener('click', () => {
+    document.querySelector('#sato-icon').setAttribute('class', 'party-color');
+  });
+});
