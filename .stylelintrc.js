f1d9c55e64460e205c456f5914ed7f3dd6c4ccdc
masarakki <masaki182@gmail.com>
2020-08-25 02:48:15 +0900

    add-sass-guidelines

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-sass-guidelines',
+  ],
   plugins: ['stylelint-order', 'stylelint-prettier'],
   rules: {
     'order/properties-alphabetical-order': true,
